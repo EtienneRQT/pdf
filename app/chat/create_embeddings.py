09f51947a3c83a1d2858dbec2fd243532d05f8e8
EtienneRQT <etienne.riquet@sfr.fr>
2024-01-11 02:49:32 +0100

    creating embeddings logic for Pinecone

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -23,3 +23,5 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
     loader = PyPDFLoader(file_path=pdf_path)
     docs = loader.load_and_split(text_splitter)
+    
+    print(docs)
