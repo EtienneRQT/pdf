8746c364d343bb0acbfca9ee10996591f162e963
EtienneRQT <etienne.riquet@sfr.fr>
2024-01-19 23:08:50 +0100

    BarChart functions missing correction

--- client/src/components/BarChart.svelte
+++ client/src/components/BarChart.svelte
@@ -64,7 +64,68 @@
         });
     });
 
-    // Functions for color generation remain the same
+function rgbToHsl(r: number, g: number, b: number) {
+		r /= 255;
+		g /= 255;
+		b /= 255;
+		let max = Math.max(r, g, b),
+			min = Math.min(r, g, b);
+		let h: number = (max + min) / 2;
+		let s: number = (max + min) / 2;
+		let l: number = (max + min) / 2;
+		if (max === min) {
+			h = s = 0; // achromatic
+		} else {
+			let diff = max - min;
+			s = l > 0.5 ? diff / (2 - max - min) : diff / (max + min);
+			switch (max) {
+				case r:
+					h = (g - b) / diff + (g < b ? 6 : 0);
+					break;
+				case g:
+					h = (b - r) / diff + 2;
+					break;
+				case b:
+					h = (r - g) / diff + 4;
+					break;
+			}
+			h /= 6;
+		}
+		return [h * 360, s * 100, l * 100];
+	}
+	function hslToRgb(h: number, s: number, l: number) {
+		let r, g, b;
+		if (s === 0) {
+			r = g = b = l; // achromatic
+		} else {
+			let hue2rgb = (p: number, q: number, t: number) => {
+				if (t < 0) t += 1;
+				if (t > 1) t -= 1;
+				if (t < 1 / 6) return p + (q - p) * 6 * t;
+				if (t < 1 / 2) return q;
+				if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+				return p;
+			};
+			let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+			let p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1 / 3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1 / 3);
+		}
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+	}
+	function generateColors(rgb: { r: number; b: number; g: number }, k: number, alpha = 1) {
+		const result = [];
+		const [h, s, l] = rgbToHsl(rgb.r, rgb.g, rgb.b);
+		const increment = 360 / k;
+		for (let i = 0; i < k; i++) {
+			let newHue = (h + increment * i) % 360;
+			let [r, g, b] = hslToRgb(newHue / 360, s / 100, l / 100);
+			result.push(`rgb(${r}, ${g}, ${b}, ${alpha})`);
+		}
+		return result;
+	}
+	const colors = generateColors({ r: 255, g: 99, b: 132 }, 7);
 </script>
 
 <canvas
