03632a7de986deba87f3e7ea1e81da5e5fa0b78c
EtienneRQT <etienne.riquet@sfr.fr>
2024-01-31 17:56:53 +0100

    added unstructured as a loader

--- app/chat/manage_embeddings.py
+++ app/chat/manage_embeddings.py
@@ -1,23 +1,27 @@
 from typing import Iterable, List
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader
+from unstructured.cleaners.core import clean_extra_whitespace
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from app.chat.vector_stores.pinecone import vector_store
 
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    # Use PyPDFLoader to load the PDF and split it into pages.
-    # TODO: Use PyMuPDFLoader instead of PyPDFLoader.
-    loader = PyMuPDFLoader(file_path=pdf_path)
+
+    loader = UnstructuredPDFLoader(
+        file_path=pdf_path, post_processors=[clean_extra_whitespace]
+    )
     docs: List[Document] = loader.load_and_split(text_splitter=text_splitter)
 
     for doc in docs:
-        doc.metadata = {
-            "page": doc.metadata["page"],
-            "text": doc.page_content,
-            "pdf_id": pdf_id,
-        }
+        doc.metadata.update(
+            {
+                "text": doc.page_content,
+                "pdf_id": pdf_id,
+            }
+        )
 
     vector_store.add_documents(documents=docs)
 
